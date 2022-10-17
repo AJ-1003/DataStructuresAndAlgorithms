9762d0bf54ef91553832d41f16617e609587dcf2
AJ-1003 <ia.jordaan1003@gmail.com>
2022-10-17 12:11:46 +0200

    Add project files.

---
+++ Stacks.CL/StacksArray.cs
@@ -0,0 +1,78 @@
+﻿namespace Stacks.CL
+{
+    public class StacksArray<T>
+    {
+        private T[] data;
+        private int top;
+
+        public StacksArray(int n)
+        {
+            data = new T[n];
+            top = 0;
+        }
+
+        public int length()
+        {
+            return top;
+        }
+
+        public bool isEmpty()
+        {
+            return top == 0;
+        }
+
+        public bool isFull()
+        {
+            return top == data.Length;
+        }
+
+        public void push(T e)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Stack is full...");
+                return;
+            }
+            else
+            {
+                data[top] = e;
+                top++;
+            }
+        }
+
+        public T? pop()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack is empty...");
+                return default;
+            }
+            else
+            {
+                var element = data[top - 1];
+                top--;
+                return element;
+            }
+        }
+
+        public T peek()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack is empty...");
+                return default;
+            }
+
+            return data[top - 1];
+        }
+
+        public void display()
+        {
+            for (var i = 0; i< top; i++)
+            {
+                Console.Write($"{data[i]} --> ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Stacks.CL/StacksLinkedList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks.CL
+{
+    public class StacksLinkedList<T>
+    {
+        private Node<T> top;
+        private int size;
+
+        public StacksLinkedList()
+        {
+            top = null;
+            size = 0;
+        }
+
+        public int length()
+        {
+            return size;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public void push(T e)
+        {
+            var newest = new Node<T>(e, null);
+            
+            if (isEmpty())
+            {
+                top = newest;
+            }
+            else
+            {
+                newest.next = top;
+                top = newest;
+            }
+
+            size++;
+        }
+
+        public T pop()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack is empty...");
+                return default;
+            }
+
+            var element = top.element;
+
+            top = top.next;
+            size--;
+
+            return element;
+        }
+
+        public T peek()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack is empty...");
+                return default;
+            }
+
+            return top.element;
+        }
+
+        public void display()
+        {
+            var p = top;
+
+            while (p != null)
+            {
+                Console.Write($"{p.element} --> ");
+                p = p.next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
